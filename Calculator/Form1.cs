5bcb5a941927277165db272136d57e5a9f28fde2
Anastasiya <girl-physicist@live.com>
2022-12-09 14:16:28 +0300

    done

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -9,102 +9,110 @@ namespace Calculator
         {
             InitializeComponent();
         }
-
-        float a, b;
-        int count;
-        bool znak = true;
+        // объявление переменных
+        float num1, num2; // числа, над которыми будут проводиться математические операции
+        string mathOperation; // переменная, для определения математической операции
+        bool sign = true; // для ввода отрицательных чисел (соответствует кнопке +-)
 
         private void button0_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 0;
+        {   //по нажатию на кнопку "0" в textBox отобразится 0 (если он до этого был пустой)
+            //или 0 добавится к уже имеющемуся там тексту (например: в textBox написано 345 и нажали "0", тогда отобразится 3450)
+            textBox1.Text += 0;
+            // запись += является оператором сложения с присваиванием.
+            // Иначе его можно было бы записать так: textBox1.Text = textBox1.Text + 0;
         }
 
         private void buttonPoint_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + ",";
+            textBox1.Text += ",";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + 1;
+            textBox1.Text += 1;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + 2;
+            textBox1.Text += 2;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + 3;
+            textBox1.Text += 3;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + 4;
+            textBox1.Text += 4;
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + 5;
+            textBox1.Text += 5;
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + 6;
+            textBox1.Text  += 6;
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + 7;
+            textBox1.Text += 7;
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + 8;
+            textBox1.Text += 8;
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + 9;
+            textBox1.Text += 9;
         }
 
         private void buttonPlusMinus_Click(object sender, EventArgs e)
         {
-            if(znak==true)
+            // устанавливает положительное или отрицательное число. По умолчанию sign == true.
+            // Т.е. при первом нажатии на кнопку "+-" в метод передастся sign == true,
+            // а, значет, в текстбокс добавится "-" перед введеннам значением. После этого  sign = false;
+            // и, значит, при следующем нажатии кнопки "+-" в метод передастся  sign = false; Т.е. знак "-" заменится на "" (пустоту)
+            if (sign == true)
             {
                 textBox1.Text = "-" + textBox1.Text;
-                znak = false;
+                sign = false;
             }
-            else if (znak==false)
+            else if (sign == false)
             {
-                textBox1.Text=textBox1.Text.Replace("-", "");
-                znak = true;
+                textBox1.Text = textBox1.Text.Replace("-", "");
+                sign = true;
             }
-            
-        }
 
+        }
+        // Метод для проведения математических операций.
+        // По нажатию на кнопки "+", "-", "*", "/" задается параметр mathOperation
         private void calculate()
         {
 
-            switch (count)
+            switch (mathOperation)
             {
-                case 1:
-                    b = a + float.Parse(textBox1.Text);
-                    textBox1.Text = b.ToString();
+                case "+":
+                    num2 = num1 + float.Parse(textBox1.Text);
+                    textBox1.Text = num2.ToString();
                     break;
 
-                case 2:
-                    b = a - float.Parse(textBox1.Text);
-                    textBox1.Text = b.ToString();
+                case "-":
+                    num2 = num1 - float.Parse(textBox1.Text);
+                    textBox1.Text = num2.ToString();
                     break;
-                case 3:
-                    b = a * float.Parse(textBox1.Text);
-                    textBox1.Text = b.ToString();
+                case "*":
+                    num2 = num1 * float.Parse(textBox1.Text);
+                    textBox1.Text = num2.ToString();
                     break;
-                case 4:
-                    b = a / float.Parse(textBox1.Text);
-                    textBox1.Text = b.ToString();
+                case "/":
+                    num2 = num1 / float.Parse(textBox1.Text);
+                    textBox1.Text = num2.ToString();
                     break;
 
                 default:
@@ -115,39 +123,39 @@ namespace Calculator
 
         private void buttonPlus_Click(object sender, EventArgs e)
         {
-            a = float.Parse(textBox1.Text);
+            num1 = float.Parse(textBox1.Text);
             textBox1.Clear();
-            count = 1;
-            label1.Text = a.ToString() + "+";
-            znak = true;
+            mathOperation = "+";
+            label1.Text = num1.ToString() + "+";
+            sign = true;
 
         }
 
         private void buttonMinus_Click(object sender, EventArgs e)
         {
-            a = float.Parse(textBox1.Text);
+            num1 = float.Parse(textBox1.Text);
             textBox1.Clear();
-            count = 2;
-            label1.Text = a.ToString() + "-";
-            znak = true;
+            mathOperation = "-";
+            label1.Text = num1.ToString() + "-";
+            sign = true;
         }
 
         private void buttonMultipl_Click(object sender, EventArgs e)
         {
-            a = float.Parse(textBox1.Text);
+            num1 = float.Parse(textBox1.Text);
             textBox1.Clear();
-            count = 3;
-            label1.Text = a.ToString() + "*";
-            znak = true;
+            mathOperation = "*";
+            label1.Text = num1.ToString() + "*";
+            sign = true;
         }
 
         private void buttonDivide_Click(object sender, EventArgs e)
         {
-            a = float.Parse(textBox1.Text);
+            num1 = float.Parse(textBox1.Text);
             textBox1.Clear();
-            count = 4;
-            label1.Text = a.ToString() + "/";
-            znak = true;
+            mathOperation = "/";
+            label1.Text = num1.ToString() + "/";
+            sign = true;
         }
 
         private void buttonEqual_Click(object sender, EventArgs e)
@@ -169,10 +177,10 @@ namespace Calculator
             textBox1.Clear();
             for (int i = 0; i < lenght; i++)
             {
-                textBox1.Text = textBox1.Text + text[i];
+                textBox1.Text += text[i];
             }
         }
 
-        
+
     }
 }
